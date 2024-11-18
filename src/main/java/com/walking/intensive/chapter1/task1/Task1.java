ad77c74796ebbf0778a379f186f3a4412d078bc4
gentrus <1111ege@mail.ru>
2024-11-18 10:34:41 +0300

    gentrus

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,14 +15,26 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 105;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        int maxAge = 122;
 
-        return null; // Заглушка. При реализации - удалить
+        if (age == 1 || (age % 10) == 1) {
+            return "Вам " + age + " год";
+        }
+        if ((age > 1 && age < 5) || (age % 10 > 1 && age % 10 < 5)) {
+            return "Вам " + age + " года";
+        }
+        if (age <= 0 || age > maxAge) {
+            return "Некорректный ввод";
+        }
+        return "Вам " + age + " лет";
     }
+
+
 }
+
