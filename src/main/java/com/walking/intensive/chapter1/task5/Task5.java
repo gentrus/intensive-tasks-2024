3fc8f8dd500a0864392aa563f6b4ad349c586dc4
gentrus <1111ege@mail.ru>
2024-11-19 15:26:53 +0300

    gentrus

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -23,9 +23,12 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getAreaByHeron(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return -1;
+        }
+        double halfPerimeter = (a + b + c) / 2;
+        return Math.sqrt(halfPerimeter * (halfPerimeter - a) * (halfPerimeter - b) * (halfPerimeter - c));
 
-        return 0; // Заглушка. При реализации - удалить
     }
 
     /**
@@ -36,9 +39,33 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getHeights(double a, double b, double c) {
-        //        Место для вашего кода
+        double[] height;
+        double maxHeight;
+        double middleHeight;
+        double minHeight;
 
-        return null; // Заглушка. При реализации - удалить
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return height = new double[0];
+        }
+        height = new double[3];
+
+        // находим высоты
+        double height1 = (2 * getAreaByHeron(a, b, c)) / a;
+        double height2 = (2 * getAreaByHeron(a, b, c)) / b;
+        double height3 = (2 * getAreaByHeron(a, b, c)) / c;
+
+        // распологаем по возрастанию
+        maxHeight = Math.max(height1, height2);
+        maxHeight = Math.max(maxHeight, height3);
+
+        minHeight = Math.min(height1, height2);
+        minHeight = Math.min(minHeight, height3);
+
+        middleHeight = (height1 + height2 + height3) - (maxHeight + minHeight);
+
+
+        return height = new double[]{minHeight, middleHeight, maxHeight};
+
     }
 
     /**
@@ -49,10 +76,36 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        double[] medians;
+        double maxMedian;
+        double middleMedian;
+        double minMedian;
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return medians = new double[0];
+        }
+        medians = new double[3];
+
+        // находим высоты
+        double median1 = (Math.sqrt(2 * Math.pow(c, 2) + 2 * Math.pow(b, 2) - Math.pow(a, 2))) / 2;
+        double median2 = (Math.sqrt(2 * Math.pow(c, 2) + 2 * Math.pow(a, 2) - Math.pow(b, 2))) / 2;
+        double median3 = (Math.sqrt(2 * Math.pow(a, 2) + 2 * Math.pow(b, 2) - Math.pow(c, 2))) / 2;
+
+        // распологаем по возрастанию
+        maxMedian = Math.max(median1, median2);
+        maxMedian = Math.max(maxMedian, median3);
+
+        minMedian = Math.min(median1, median2);
+        minMedian = Math.min(minMedian, median3);
+
+        middleMedian = (median1 + median2 + median3) - (maxMedian + minMedian);
+
+
+        return medians = new double[]{minMedian, middleMedian, maxMedian};
+
     }
+
 
     /**
      * Реализуйте метод, который будет возвращать биссектрисы треугольника по возрастанию.
@@ -62,9 +115,34 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getBisectors(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        double[] bisectors;
+        double maxBisector;
+        double middleBisector;
+        double minBisector;
+
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return bisectors = new double[0];
+        }
+        bisectors = new double[3];
+
+        // находим высоты
+        double bisectors1 = (Math.sqrt(a*b* (a+b+c)* (a+b-c))) / (a+b);
+        double bisectors2 = (Math.sqrt(b*c* (b+c+a)* (b+c-a))) / (b+c);
+        double bisectors3 = (Math.sqrt(c*a* (c+a+b)* (c+a-b))) / (c+a);
+
+        // распологаем по возрастанию
+        maxBisector = Math.max(bisectors1, bisectors2);
+        maxBisector = Math.max(maxBisector, bisectors3);
+
+        minBisector = Math.min(bisectors1, bisectors2);
+        minBisector = Math.min(minBisector, bisectors3);
+
+        middleBisector = (bisectors1 + bisectors2 + bisectors3) - (maxBisector + minBisector);
+
+
+        return bisectors = new double[]{minBisector, middleBisector, maxBisector};
+
     }
 
     /**
