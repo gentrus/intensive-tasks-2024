0492c91207c787dfe7c33c9ad765bf003ac5bf89
gentrus <1111ege@mail.ru>
2024-11-19 12:53:37 +0300

    gentrus

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -28,17 +28,30 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        double a = 25;
+        double b = 60;
+        double c = 36;
 
         System.out.println(solveEquation(a, b, c));
 
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a==0&&b==0&&c==0) {
+            return "Бесконечное множество решений";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double discriminant= Math.pow(b,2) - (4 * a * c);
+
+        if (discriminant==0) {
+            return "Количество решений: 1. Корень: " + -b/(2*a);
+        }
+
+        if (discriminant>0) {
+            return "Количество решений: 2. Корни: " + (-b-Math.sqrt(discriminant))/(2*a) + (-b+Math.sqrt(discriminant))/(2*a) ;
+        }
+
+
+        return "Количество решений: 0";
     }
 }
