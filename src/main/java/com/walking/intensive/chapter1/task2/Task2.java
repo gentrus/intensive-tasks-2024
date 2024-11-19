e0b8caca429b50b2b8ae9a0b44f8e8f4aeb274a1
gentrus <1111ege@mail.ru>
2024-11-19 07:58:15 +0300

    gentrus

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -33,12 +33,57 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        System.out.println(getFlatLocation(3, 2, 16));
+
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int maxFlatNumber = floorAmount * entranceAmount * 4;
+        int flatOneEntrance = floorAmount * 4;
+
+        if (flatNumber <= 0 || floorAmount <= 0 || entranceAmount <= 0) {
+            return "Некорректные входные данные";
+        }
+        if (flatNumber > maxFlatNumber) {
+            return "Такой квартиры не существует";
+        }
+
+        // номер подъезда
+        int entranceNumber = flatNumber / flatOneEntrance;
+        if (flatNumber / maxFlatNumber < 1) {
+            entranceNumber++;
+        }
+
+        // номер этажа без учета разделения на подъезды
+        int commonFloorNumber = flatNumber / 4;
+        if (flatNumber % 4 != 0) {
+            commonFloorNumber++;
+        }
+
+        // номер этажа с учетом разделения на подъезды
+        int floorNumber = commonFloorNumber - floorAmount * (entranceNumber - 1);
+        // положение квартиры относительно этажа (1,2,3,4)
+        int direction = flatNumber - (commonFloorNumber - 1) * 4;
+
+
+        String direction1 = "слева от лифта, ";
+        String direction3 = "влево";
+        String direction4 = "вправо";
+        String direction2 = "справа от лифта, ";
+
+        switch (direction) {
+            case 1:
+                return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + direction1 + direction3;
+            case 2:
+                return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + direction1 + direction4;
+            case 3:
+                return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + direction2 + direction3;
+            case 4:
+                return flatNumber + " кв - " + entranceNumber + " подъезд, " + floorNumber + " этаж, " + direction2 + direction4;
+
+        }
+        return "";
     }
 }
