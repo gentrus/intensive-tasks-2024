ad7c7e4f09e9ef699b32fc154f4c4ca62f144b12
gentrus <1111ege@mail.ru>
2024-11-19 19:01:13 +0300

    gentrus

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -27,31 +27,59 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 25;
-        double b = 60;
-        double c = 36;
+
+        double a = -1;
+        double b = 3;
+        double c = 4;
 
         System.out.println(solveEquation(a, b, c));
 
     }
 
     static String solveEquation(double a, double b, double c) {
-        if (a==0&&b==0&&c==0) {
+        if (a == 0 && b == 0 && c == 0) {
             return "Бесконечное множество решений";
         }
 
-        double discriminant= Math.pow(b,2) - (4 * a * c);
-
-        if (discriminant==0) {
-            return "Количество решений: 1. Корень: " + -b/(2*a);
+        // Неполные квадратные уравнения
+        if (a != 0 && b == 0 && c == 0) {
+            return "Количество решений: 1. Корень: 0";
+        }
+        if (a == 0 && b == 0) {
+            return "Некорректные входные данные";
+        }
+        if (a == 0 && c != 0) {
+            return "Количество решений: 1. Корень: " + (int) (-c / b);
+        }
+        if (a != 0 && b == 0) {
+            if (c / a < 0) {
+                return "Некорректные входные данные";
+            } else {
+                if (c == a) {
+                    return "Количество решений: 0.";
+                }
+                return "Количество решений: 2. Корни: " + (int) (Math.sqrt(c)) + ";" + (int) (-Math.sqrt(c));
+            }
+        }
+        if (a != 0 && c == 0) {
+            return "Количество решений: 2. Корни: " + 0 + ";" + b / a;
         }
 
-        if (discriminant>0) {
-            return "Количество решений: 2. Корни: " + (-b-Math.sqrt(discriminant))/(2*a) + (-b+Math.sqrt(discriminant))/(2*a) ;
+        // Дискриминант
+        double discriminant = Math.pow(b, 2) - (4 * a * c);
+
+        if (discriminant == 0) {
+            return "Количество решений: 1. Корень: " + (int) (-b / (2 * a));
+        }
+
+        if (discriminant > 0) {
+            if (((-b + Math.sqrt(discriminant)) / (2 * a)) > ((-b - Math.sqrt(discriminant)) / (2 * a))) {
+                return "Количество решений: 2. Корни: " + (int) ((-b + Math.sqrt(discriminant)) / (2 * a)) + ";" + (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
+            }
+            return "Количество решений: 2. Корни: " + (int) ((-b - Math.sqrt(discriminant)) / (2 * a)) + ";" + (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
         }
 
 
-        return "Количество решений: 0";
+            return "Количество решений: 0.";
+        }
     }
-}
