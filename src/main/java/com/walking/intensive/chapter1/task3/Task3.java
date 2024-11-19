d43b546d4100d109b5c22b39948d1af8ebbcc18b
gentrus <1111ege@mail.ru>
2024-11-19 10:50:42 +0300

    gentrus

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -25,12 +25,27 @@ package com.walking.intensive.chapter1.task3;
  */
 public class Task3 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        System.out.println(isLeap(404));
+
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
+        if (year <= 0) {
+            return false;
+        }
 
-        return false; // Заглушка. При реализации - удалить
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
