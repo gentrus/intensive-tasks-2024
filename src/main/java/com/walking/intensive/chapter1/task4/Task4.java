b09880779765e21235fe40f6c20f48ad45747a71
gentrus <1111ege@mail.ru>
2024-11-21 23:24:36 +0300

    Исправил)

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -40,16 +40,14 @@ public class Task4 {
         if (a == 0 && b == 0 && c == 0) {
             return "Бесконечное множество решений";
         }
+
         if (a == 0 && b == 0) {
             return "Некорректные входные данные";
         }
         if (a == 0) {
             return "Количество решений: 1. Корень: " + (-c / b);
         }
-        if (b==0&&c==0) {
-            return "Количество решений: 1. Корень: " + 0;
-        }
-        if (b == 0) {
+        if (b == 0 && c != 0) {
             if (c / a < 0) {
                 return "Некорректные входные данные";
             }
@@ -58,24 +56,24 @@ public class Task4 {
             }
             return "Количество решений: 2. Корни: " + (Math.sqrt(c)) + ";" + (-Math.sqrt(c));
         }
-        if (c == 0) {
+        if (c == 0 && b != 0) {
             return "Количество решений: 2. Корни: " + 0 + ";" + b / a;
         }
 
         // Дискриминант
         double discriminant = Math.pow(b, 2) - (4 * a * c);
-
         if (discriminant == 0) {
+            if ((-b / (2 * a))==0) {
+                return "Количество решений: 1. Корень: " + 0;
+            }
             return "Количество решений: 1. Корень: " + (-b / (2 * a));
         }
-
         if (discriminant > 0) {
             if (((-b + Math.sqrt(discriminant)) / (2 * a)) > ((-b - Math.sqrt(discriminant)) / (2 * a))) {
                 return "Количество решений: 2. Корни: " + ((-b + Math.sqrt(discriminant)) / (2 * a)) + ";" + ((-b - Math.sqrt(discriminant)) / (2 * a));
             }
             return "Количество решений: 2. Корни: " + ((-b - Math.sqrt(discriminant)) / (2 * a)) + ";" + ((-b + Math.sqrt(discriminant)) / (2 * a));
         }
-
 
         return "Количество решений: 0.";
     }
