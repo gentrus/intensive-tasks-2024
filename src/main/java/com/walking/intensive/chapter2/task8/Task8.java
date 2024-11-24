4b353ed4124545c8cc6ba5a16759d7129c0cad5f
egor.djun <xedshotg@gmail.com>
2024-11-24 14:06:21 +0700

    task 8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -20,11 +20,35 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(getHappyTicketChance());
     }
 
     static double getHappyTicketChance() {
-        // Ваш код
-        return 0.0;
+        double result = 0.0;
+
+        for (int i = 0; i < 1_000_000; i++) {
+            if (isHappyTicket(i)) {
+                result++;
+            }
+        }
+
+        return result / 1_000_000;
+    }
+
+    private static boolean isHappyTicket(int ticket) {
+        int sum = 0;
+        int sumTwo = 0;
+
+        for (int i = 0; i < 3; i++) {
+            sum += ticket % 10;
+            ticket /= 10;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            sumTwo += ticket % 10;
+            ticket /= 10;
+        }
+
+        return sum == sumTwo;
     }
 }
