757cdc9a090579d097c26b2ede68ddec45fd1b3e
gentrus <1111ege@mail.ru>
2024-11-25 15:27:41 +0300

    1

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -11,11 +11,34 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+        if (inputString==null||inputString.length()<2) {
+            return false;
+        }
+
+        int letterNumb2 = inputString.length() - 1;
+        for (int letterNumb1 = 0; letterNumb1 < inputString.length() / 2; ) {
+            char letter1 = inputString.charAt(letterNumb1);
+            while (letter1 == ' ' || letter1 == '?' || letter1 == '!' || letter1 == ',' || letter1 == '-') {
+                letterNumb1++;
+                letter1 = inputString.charAt(letterNumb1);
+            }
+            char letter2 = inputString.charAt(letterNumb2);
+            while (letter2 == ' ' || letter2 == '?' || letter2 == '!' || letter2 == ',' || letter2 == '-') {
+                letterNumb2--;
+                letter2 = inputString.charAt(letterNumb2);
+            }
+
+            if (Character.toUpperCase(letter1) != Character.toUpperCase(letter2)) {
+                return false;
+            }
+            letterNumb1++;
+            letterNumb2--;
+        }
+
+        return true;
     }
 }
